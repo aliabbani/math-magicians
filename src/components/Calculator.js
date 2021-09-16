341e5456f30f6ac51fb79f1a38d16f87d3aa7e8f
ali abbani <aliox@ahlam.com>
2021-09-16 17:09:10 +0300

    Refactor the Calculator component (components/Calculator.js) from a class based component to a functional component with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,53 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
+function Calculator() {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-    this.clickButton = this.clickButton.bind(this);
+  function clickButton(e) {
+    setState((state) => calculate(state, e.target.innerHTML));
   }
-
-  clickButton(e) {
-    this.setState((state) => calculate(state, e.target.innerHTML));
-  }
-
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div className="calc-container">
-        <div className="result">
-          <div>{ total }</div>
-          <div>{ operation }</div>
-          <div>{ next }</div>
-        </div>
-        <button type="button" name="AC" onClick={this.clickButton} className="gButton">AC</button>
-        <button type="button" name="+/-" onClick={this.clickButton} className="gButton">+/-</button>
-        <button type="button" name="%" onClick={this.clickButton} className="gButton">%</button>
-        <button type="button" name="÷" onClick={this.clickButton} className="oButton">÷</button>
-        <button type="button" name="7" onClick={this.clickButton} className="gButton">7</button>
-        <button type="button" name="8" onClick={this.clickButton} className="gButton">8</button>
-        <button type="button" name="9" onClick={this.clickButton} className="gButton">9</button>
-        <button type="button" name="x" onClick={this.clickButton} className="oButton">x</button>
-        <button type="button" name="4" onClick={this.clickButton} className="gButton">4</button>
-        <button type="button" name="5" onClick={this.clickButton} className="gButton">5</button>
-        <button type="button" name="6" onClick={this.clickButton} className="gButton">6</button>
-        <button type="button" name="-" onClick={this.clickButton} className="oButton">-</button>
-        <button type="button" name="1" onClick={this.clickButton} className="gButton">1</button>
-        <button type="button" name="2" onClick={this.clickButton} className="gButton">2</button>
-        <button type="button" name="3" onClick={this.clickButton} className="gButton">3</button>
-        <button type="button" name="+" onClick={this.clickButton} className="oButton">+</button>
-        <button type="button" name="0" onClick={this.clickButton} className="gButton zero">0</button>
-        <button type="button" name="." onClick={this.clickButton} className="gButton">.</button>
-        <button type="button" name="=" onClick={this.clickButton} className="oButton">=</button>
+  return (
+    <div className="calc-container">
+      <div className="result">
+        <div>{ state.total }</div>
+        <div>{ state.operation }</div>
+        <div>{ state.next }</div>
       </div>
-    );
-  }
+      <button type="button" name="AC" onClick={clickButton} className="gButton">AC</button>
+      <button type="button" name="+/-" onClick={clickButton} className="gButton">+/-</button>
+      <button type="button" name="%" onClick={clickButton} className="gButton">%</button>
+      <button type="button" name="÷" onClick={clickButton} className="oButton">÷</button>
+      <button type="button" name="7" onClick={clickButton} className="gButton">7</button>
+      <button type="button" name="8" onClick={clickButton} className="gButton">8</button>
+      <button type="button" name="9" onClick={clickButton} className="gButton">9</button>
+      <button type="button" name="x" onClick={clickButton} className="oButton">x</button>
+      <button type="button" name="4" onClick={clickButton} className="gButton">4</button>
+      <button type="button" name="5" onClick={clickButton} className="gButton">5</button>
+      <button type="button" name="6" onClick={clickButton} className="gButton">6</button>
+      <button type="button" name="-" onClick={clickButton} className="oButton">-</button>
+      <button type="button" name="1" onClick={clickButton} className="gButton">1</button>
+      <button type="button" name="2" onClick={clickButton} className="gButton">2</button>
+      <button type="button" name="3" onClick={clickButton} className="gButton">3</button>
+      <button type="button" name="+" onClick={clickButton} className="oButton">+</button>
+      <button type="button" name="0" onClick={clickButton} className="gButton zero">0</button>
+      <button type="button" name="." onClick={clickButton} className="gButton">.</button>
+      <button type="button" name="=" onClick={clickButton} className="oButton">=</button>
+    </div>
+  );
 }
 
 export default Calculator;
